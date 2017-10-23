2410317027e1a4300f06f77ceddb167845f1bae8
taowu750 <2691320794@qq.com>
2017-10-23 17:36:59 +0800

    添加上次遗漏的应用程序级别的包

---
+++ app/src/main/java/com/coolweather/android/app/BaseActivity.java
@@ -0,0 +1,92 @@
+package com.coolweather.android.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.coolweather.android.R;
+import com.coolweather.android.util.ActivityCollector;
+import com.coolweather.android.util.LogUtil;
+
+/**
+ * 项目基础的 Activity 类，用来提供一些特殊功能。它会在每次创建时在日志中打印类名，其中 tag 为"BaseActivity"<br/>
+ * 还提供了强制结束应用的方法。
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    public static final String FORCE_FINISH = "com.coolweather.android.FORCE_FINISH";
+
+
+    private ForceOfflineReceiver receiver;
+    private LocalBroadcastManager localBroadcastManager;
+
+    private static final String TAG = "BaseActivity";
+
+
+    /**
+     * 当活动处于活动栈栈顶时，该方法将会弹出一个对话框询问用户是否终结应用
+     */
+    public void forceFinish() {
+        if (receiver != null) {
+            Intent intent = new Intent(FORCE_FINISH);
+            localBroadcastManager.sendBroadcast(intent);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LogUtil.d(TAG, getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(FORCE_FINISH);
+        receiver = new ForceOfflineReceiver();
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        localBroadcastManager.registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver != null) {
+            localBroadcastManager.unregisterReceiver(receiver);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("警告");
+            builder.setMessage(R.string.app_name + "将被强制终结，是否继续？");
+            builder.setCancelable(false);
+            builder.setPositiveButton("是", (dialog, which) -> {
+                ActivityCollector.finishAll();
+                Toast.makeText(context, R.string.app_name + "已经结束！", Toast.LENGTH_SHORT).show();
+            });
+            builder.setNegativeButton("否", null);
+            builder.show();
+        }
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/app/BaseApplication.java
@@ -0,0 +1,28 @@
+package com.coolweather.android.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.litepal.LitePal;
+
+/**
+ * 项目基础的 Application 类，可以提供全局的 Context 对象。
+ */
+
+public class BaseApplication extends Application {
+
+    private static Context context;
+
+
+    public static Context getContext() {
+        return context;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        LitePal.initialize(context);
+    }
+}
